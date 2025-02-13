47946e0a5e93cbaa1ebe23d8e494838c2eeea53e
Christian Nuss <christian@ybor.ai>
2025-02-13 09:55:13 -0500

    azure fixes

--- src/models/azure.rs
+++ src/models/azure.rs
@@ -4,20 +4,25 @@ use std::fmt::{self, Display, Formatter};
 #[derive(Serialize, Deserialize, Clone, Debug)]
 #[serde(rename_all = "camelCase")]
 pub struct AzureConfig {
-    pub cloud_name: String,
-    pub home_tenant_id: String,
+    pub cloud_name: Option<String>,
+    pub home_tenant_id: Option<String>,
     pub id: String,
-    pub is_default: bool,
+    pub is_default: Option<bool>,
     // pub managedByTenants: Vec<Tenant>,
-    pub name: String,
-    pub state: AzureAccountState,
-    pub tenant_id: String,
+    pub name: Option<String>,
+    pub state: Option<AzureAccountState>,
+    pub tenant_id: Option<String>,
     // pub user: AzureUser,
 }
 
 impl Display for AzureConfig {
     fn fmt(&self, f: &mut Formatter) -> fmt::Result {
-        write!(f, "AzureConfig: {}({})", self.name, self.id)
+        write!(
+            f,
+            "AzureConfig: {}({})",
+            self.name.clone().unwrap_or_default(),
+            self.id
+        )
     }
 }
 
